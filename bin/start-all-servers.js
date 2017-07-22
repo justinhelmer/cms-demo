e151f7a642a3f5cbf0ea45e0b464a5fa0776c27b
Justin Helmer <jhelmer25@gmail.com>
2017-07-22 12:10:49 -0700

    suppress mongo output on startup

--- bin/start-all-servers.js
+++ bin/start-all-servers.js
@@ -1,14 +1,20 @@
 const forever = require('forever-monitor');
 const path = require('path');
+
+const mongo = require('../config/keystone.config').mongo;
 let processes = {};
 
 processes.db = forever.start(['mongod'], {
   max: 1,
-  silent: false
+  silent: true
 });
 
 processes.server = new (forever.Monitor)(path.resolve(__dirname, '../server/index.js'), {
   max: 1
+});
+
+processes.db.on('start', function () {
+  console.log('MongoDB running:', mongo);
 });
 
 processes.db.on('exit', function () {
