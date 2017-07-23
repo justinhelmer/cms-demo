71ae5ea333cc23e5dea7465b1156dcf61098dca0
Justin Helmer <jhelmer25@gmail.com>
2017-07-23 02:01:47 -0700

    add button to copy windows path to clipboard

--- src/components/Video.vue
+++ src/components/Video.vue
@@ -1,16 +1,25 @@
 <template>
-    <div id="video" :class="$style.wrapper">
+    <div id="video">
         <h1>{{video.name}}</h1>
 
         <div :class="$style.player">
-          <video controls>
+          <video controls :class="$style.player">
             <source :src="video.path" type="video/mp4">
           </video>
+        </div>
+
+        <div :class="$style['below-player']">
+          <button class="btn" :data-clipboard-text="  windowsPath">Copy path</button>
         </div>
     </div>
 </template>
 
 <script>
+  if (!require('../lib/is-server')()) {
+    const Clipboard = require('clipboard');
+    new Clipboard('.btn');
+  }
+
   export default {
     name: 'video',
 
@@ -25,6 +34,12 @@
     computed: {
       video () {
         return this.$store.getters.getItemById(this.$route.params.id, 'videos');
+      },
+
+      windowsPath () {
+        return this.$store.getters.getItemById(this.$route.params.id, 'videos').path
+          .replace('/mnt/a', 'A:')
+          .replace(/\//g, '\\');
       }
     }
   }
@@ -32,22 +47,18 @@
 
 <style module>
     @value max from '../css/breakpoints.css';
+    @value normal as m-normal from '../css/layout.css';
 
-    .wrapper {
-      display: flex;
-      flex-direction: column;
+    .player {  
+      display: block;
+      margin: 0 auto;
+      max-width: max;
+      height: auto;
+      width: 100%;
     }
 
-    .player {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      flex-grow: 1;
-
-      & video {  
-        height: auto;
-        max-width: max;
-        width: 100%;
-      }
+    .below-player {
+      margin: m-normal auto;
+      max-width: max;
     }
 </style>
